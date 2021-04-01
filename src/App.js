40c29fa26000f00bd1a872bccd03b796d98d891a
Karin Kazarian <karinghazarian@gmail.com>
2021-04-01 01:10:01 -0400

    Add map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div className='App'></div>;
-}
+import Map from './components/Map.jsx';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>App Name</h1>
+      <Map />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+const Map = () => {
+  const [viewport, setViewport] = useState({
+    width: '100%',
+    height: 400,
+    latitude: 45.537875,
+    longitude: -73.757928,
+    zoom: 8,
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle='mapbox://styles/karin-kazarian/ckmyepr931r7317prtkdbrhj1'
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    />
+  );
+};
+
+export default Map;
