79b48e84912dc825097584afdadb35185de8b9ae
Karin Kazarian <karinghazarian@gmail.com>
2021-04-01 14:56:59 -0400

    test deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Map from './components/Map.jsx';
 const App = () => {
   return (
     <div className='App'>
-      <h1>App Name</h1>
+      <h1>App Name changed</h1>
       <Map />
     </div>
   );
