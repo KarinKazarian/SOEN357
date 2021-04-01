3f6bdc3df091df29107956140cb3e91093c04d6c
Karin Kazarian <karinghazarian@gmail.com>
2021-04-01 16:27:07 -0400

    automatic deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Map from './components/Map.jsx';
 const App = () => {
   return (
     <div className='App'>
-      <h1>App Name changed</h1>
+      <h1>App Name changed test deploy</h1>
       <Map />
     </div>
   );
