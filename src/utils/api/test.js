583446b8162401205b061a7751e05ce7a0503009
Sujan Saravanamuthu <sujansaravanamuthu@gmail.com>
2021-04-11 19:49:51 -0400

    remove useless code - Karin 2021

--- src/utils/api/test.js
+++
@@ -1,9 +0,0 @@
-import { makeGetRequest, makePostRequest} from './base.js';
-const ENDPOINT ='users'
-const getTest = async () => {
-  return makeGetRequest('');
-};
-const postTest = async (body) => {
-  return makePostRequest(ENDPOINT,body);
-};
-export { getTest, postTest };
